7ff21c7c08ab07d6fc6692c61e2176990852ea15
Jagruth Gajula <jagruth.gajula@gmail.com>
2026-09-23 21:41:35 -0700

    chore: bump version to v1.3.1

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -11,7 +11,7 @@ import (
 	"github.com/Ryoshkenn/zap/internal/ui"
 )
 
-var Version = "v1.3.0"
+var Version = "v1.3.1"
 
 // NewRootCmd builds the cobra root with all subcommands wired in.
 func NewRootCmd() *cobra.Command {
